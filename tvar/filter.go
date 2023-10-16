0af1b8e57192ff8007b0ac5c5e7f39dbbb335330
goodliu <liuzengh@qq.com>
2023-10-16 20:05:56 +0800

    run go fmt ./... (#3)

--- tvar/filter.go
+++ tvar/filter.go
@@ -1,8 +1,15 @@
+//
+//
 // Tencent is pleased to support the open source community by making tRPC available.
-// Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
 // If you have downloaded a copy of the tRPC source code from Tencent,
 // please note that tRPC source code is licensed under the Apache 2.0 License,
 // A copy of the Apache 2.0 License is included in this file.
+//
+//
 
 // Package tvar 对rpc请求量、成功量、失败量、耗时分布、qps、百分位数进行统计上报
 package tvar
